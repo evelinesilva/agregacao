bc00d2bdac41ea867e42062fd8020a0c11718e7f
php 11 <php.11@genio.cec.ime.usp.br>
2018-01-24 20:15:04 -0200

    ok

--- testaCliente.php
+++ testaCliente.php
@@ -1,6 +1,22 @@
-<<?php
+<?php
 
-    $cliente1 = new cliente
+    require "Cliente.php";
+    require "cartaoDeCredito.php";
+
+
+    $cliente1 = new Cliente();
+    $cliente1->nome = "Joana";
+    $cliente1->email = "joana@email";
+
+    $cartao1 = new cartaoDeCredito();
+    $cartao1->cliente = $cliente1;
+    $cartao1->numero = 123456;
+    $cartao1->dataDeValidade = "01/03/2016";
+
+    echo ("Testando Classe Cliente");
+
+    echo ("$cliente1->nome");
+
 
 
  ?>
